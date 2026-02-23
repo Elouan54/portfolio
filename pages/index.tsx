41dd013c355a574c592c4fb203c1a29eb7830bb3
Elouan <elouan.jeanson@gmail.com>
2026-02-23 21:07:36 +0100

    feat: mettre à jour le contenu de la page d'accueil et ajouter un lien vers le code GitHub

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,8 @@ export default function Home() {
           <h1 className="text-5xl font-bold mb-4">Elouan Jeanson</h1>
           <h2 className="text-2xl text-gray-400 mb-6">Analyste Développeur</h2>
           <p className="max-w-2xl text-gray-300">
-            Je conçois et développe des applications performantes et évolutives, avec une vision architecturale des systèmes d’information.
+            Mon CV et mes futurs projets sur différentes technologies (en construction) sont disponibles sur cette page.
           </p>
-          <a
-            href="https://github.com/Elouan54/portfolio"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          >
-            Voir le code sur GitHub
-          </a>
         </section>
 
         {/* APPLICATIONS */}
@@ -24,20 +16,30 @@ export default function Home() {
             <h3 className="text-3xl font-semibold mb-10 text-center">Mes Applications</h3>
             <div className="grid md:grid-cols-3 gap-8">
               <div className="bg-gray-800 p-6 rounded-xl hover:scale-105 transition">
+                <h4 className="text-xl font-semibold mb-2">Next.js</h4>
+                <p className="text-gray-400 mb-2">Cette page web</p>
+                <a
+                  href="https://github.com/Elouan54/portfolio"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="mt-6 px-4 py-2 bg-blue-500 text-white rounded mb-2 hover:bg-blue-600"
+                >
+                  Voir le code sur GitHub
+                </a>
+              </div>
+              <div className="bg-gray-800 p-6 rounded-xl hover:scale-105 transition">
                 <h4 className="text-xl font-semibold mb-2">Symfony + VueJS</h4>
-                <p className="text-gray-400">Application web</p>
+                <p className="text-gray-400">En construction</p>
               </div>
               <div className="bg-gray-800 p-6 rounded-xl hover:scale-105 transition">
                 <h4 className="text-xl font-semibold mb-2">Flutter</h4>
-                <p className="text-gray-400">Applications Flutter</p>
-              </div>
-              <div className="bg-gray-800 p-6 rounded-xl">
-                <h4 className="text-xl font-semibold mb-2">Autres Projets</h4>
-                <p className="text-gray-400">Futur projet...</p>
+                <p className="text-gray-400">En construction</p>
               </div>
             </div>
           </div>
         </section>
+
+        
 
         {/* CONTACT */}
         <section className="py-20 px-6 bg-gray-950 text-white text-center">
